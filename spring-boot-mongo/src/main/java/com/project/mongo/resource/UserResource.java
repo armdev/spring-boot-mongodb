24a06de99fc0b49d60aedc6db72178359a7932ee
ARMEN ARZUMANYAN <armen.arzumanyan.external@atos.net>
2016-12-19 18:19:57 +0400

    Spring Boot MongoDB

--- spring-boot-mongo/src/main/java/com/project/mongo/resource/UserResource.java
+++ spring-boot-mongo/src/main/java/com/project/mongo/resource/UserResource.java
@@ -20,25 +20,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/user")
 @Produces(MediaType.APPLICATION_JSON_VALUE + ";charset=utf-8")
 public class UserResource {
-    
-     @Autowired
-     private CustomerService customerService;
-     
+
+    @Autowired
+    private CustomerService customerService;
 
     //access http://localhost:9999/api/user/username/?searchkey=armen
     @RequestMapping(method = RequestMethod.GET, value = "/username", params = {"searchkey"})
     public Response findPerson(@RequestParam(value = "searchkey") String searchkey) {
-        System.out.println("Username is " +searchkey);
+        System.out.println("Username is " + searchkey);
         return Response.ok("ok").type(javax.ws.rs.core.MediaType.APPLICATION_JSON).build();
     }
+
     // access http://localhost:9999/api/user/all
     @RequestMapping(method = RequestMethod.GET, value = "/all")
     public Response findAllCostumers() {
-       Customer obj = new Customer();
-       obj.setFirstName("UserFname"+System.currentTimeMillis());
-       obj.setLastName("UserLname"+System.currentTimeMillis());
-       customerService.save(obj);       
-       List<Customer> list =  customerService.findAll();        
-       return Response.ok().entity(list).type(javax.ws.rs.core.MediaType.APPLICATION_JSON + ";charset=utf-8").build();
+        Customer obj = new Customer();
+        obj.setFirstName("UserFname" + System.currentTimeMillis());
+        obj.setLastName("UserLname" + System.currentTimeMillis());
+        customerService.save(obj);
+        List<Customer> list = customerService.findAll();
+        return Response.ok().entity(list).type(javax.ws.rs.core.MediaType.APPLICATION_JSON + ";charset=utf-8").build();
     }
 }
